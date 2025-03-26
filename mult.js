3f5026c5005f57fc68f79ba9c1e3dd4c681d8191
nazim11062003 <139106991+nazim11062003@users.noreply.github.com>
2025-03-26 11:40:43 +0530

    addition class added mult.js

--- mult.js
+++ mult.js
@@ -1,3 +1,13 @@
+class Addition {
+    constructor(num1, num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    calculate() {
+        return this.num1 + this.num2;
+    }
+}
 class Multiplication {
     constructor(num1, num2) {
         this.num1 = num1;
@@ -8,6 +18,8 @@ class Multiplication {
         return this.num1 * this.num2;
     }
 }
+const add = new Addition(5, 10);
+console.log("Addition Result:", add.calculate());
 
 const multiply = new Multiplication(5, 10);
 console.log("Multiplication Result:", multiply.calculate());
