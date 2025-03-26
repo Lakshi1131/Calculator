78ce6575a4adef707ee4843826230a7b361d2784
Pawan-04 <pawan4kp@gmail.com>
2025-03-26 11:57:45 +0530

    Update mult.js

--- mult.js
+++ mult.js
@@ -18,6 +18,21 @@ class Multiplication {
         return this.num1 * this.num2;
     }
 }
+
+class Subtraction {
+    constructor(num1, num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    calculate() {
+        return this.num1 - this.num2;
+    }
+}
+
+const subtract = new Subtraction(10, 5);
+console.log("Subtraction Result:", subtract.calculate()); // Output: 5
+
 const add = new Addition(5, 10);
 console.log("Addition Result:", add.calculate());
 
